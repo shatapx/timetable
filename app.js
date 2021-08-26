2825da5a30737dc91b74e0f3de26986fbcb62842
Prime <lammer2105@gmail.com>
2021-08-26 19:51:16 +0300

    main files

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = config.get("port");
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Started on ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error", errror.message);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useRoutes } from "./routes";
+import "materialize-css";
+
+function App() {
+  const routes = useRoutes(false);
+  return (
+    <Router>
+      <div className="container">{routes}</div>
+    </Router>
+  );
+}
+
+export default App;
