cd34d0c2e76b0b81cd28bebe9260a8a19285aa0f
Prime <lammer2105@gmail.com>
2021-08-27 19:32:30 +0300

    change error text, removed some fields from User

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,51 +1,47 @@
-const { Router } = require("express");
+const { Router, json } = require("express");
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = Router();
 const jwt = require("jsonwebtoken");
+
 router.post(
   "/registration",
   [
-    check("email", "Incorrect email").isEmail(),
-    check("password", "Short password").isLength({ min: 8 }),
-    check("name", "Empty name!").exists(),
-    check("course", "Choose course!").exists(),
-    check("op", "Choose educational program").exists(),
+    check("email", "Некоректний email").normalizeEmail().isEmail(),
+    check("password", "Закороткий пароль").isLength({ min: 8 }),
   ],
   async (request, response) => {
     try {
-      console.log("Body: ", req.body);
       const errors = validationResult(request);
       if (!errors.isEmpty()) {
         return response.status(400).json({
           errors: errors.array(),
-          message: "Incorrect registration data",
+          message:
+            "Некоректний email або пароль. Пароль повинен бути не менше 8 символів",
         });
       }
 
-      const { email, password, name, course, op } = request.body;
+      const { email, password } = request.body;
       const candidate = await User.findOne({ email });
 
       if (candidate)
         return response
           .status(400)
-          .json({ message: "such a user already exists" });
+          .json({ message: "Такий користувач вже існує в системі" });
 
       const hashedPassword = await bcrypt.hash(password, 12);
       const user = new User({
         email,
         password: hashedPassword,
-        name,
-        course,
-        op,
       });
       await user.save();
-
-      response.status(201).json({ message: "User registered" });
+      response.status(201).json({ message: "Зареєстровано" });
     } catch (error) {
-      response.status(500).json({ message: "Error. Try again" });
+      response
+        .status(500)
+        .json({ message: "Помилка. Спробуйте знову через деякий час" });
     }
   }
 );
@@ -53,7 +49,7 @@ router.post(
 router.post(
   "/login",
   [
-    check("email", "Enter the correct email").normalizeEmail().isEmail(),
+    check("email", "Incorrect email").normalizeEmail().isEmail(),
     check("password", "Enter password").exists(),
   ],
   async (request, response) => {
@@ -67,22 +63,19 @@ router.post(
       }
       const { email, password } = request.body;
       const user = await User.findOne({ email });
-
-      if (!user)
-        return response.status(400).json({ message: "User not found " });
+      const error_message = "Невірний email або пароль";
+      if (!user) return response.status(400).json({ message: error_message });
 
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch)
-        return response
-          .status(400)
-          .json({ message: "Incorrect password, try again" });
+        return response.status(400).json({ message: error_message });
 
       const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
         expiresIn: "48h",
       });
-      response.json({ token, userId: user.id });
+      response.json({ token, userId: user.id, message: "Авторизовано" });
     } catch (error) {
-      response.status(500).json({ message: "Error. Try again" });
+      response.status(500).json({ message: "Помилка. Спробуйте знову" });
     }
   }
 );
